419901bd28e54548cfca37ecff0b73a53097c09e
Nitish Kumar <imsanganakabhiyanta@gmail.com>
2022-01-21 20:37:57 +0530

    Learn all about ListTile and divider as a child in the sizedBox.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,91 +15,140 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: const [
               CircleAvatar(
                 radius: 50.0,
-                // backgroundColor:Colors.red ,
+                // // backgroundColor:Colors.red ,
                 backgroundImage: AssetImage('assets/images/art.png'),
               ),
               Text(
-                'Nitish',
+                'Nitish and Neelam',
                 style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Pacifico',
-                ),
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
               ),
               Text(
-                'Flutter Developer',
+                'FLUTTER DEVELOPER',
                 style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Pacifico',
-                  letterSpacing: 2.0,
+                  // wordSpacing: 55.0
+                    color: Colors.white,
+                    // fontWeight: FontWeight.bold,
+                    fontFamily: 'Shrikhand',
+                    fontSize: 20.0,
+                    letterSpacing: 2.0),
+              ),
+              SizedBox(
+                height: 20.0,
+                child: Divider(
+                  color: Colors.white38,
                 ),
               ),
-
               Card(
-                color: Colors.white,
+                // card is always white by default
+                // color: Colors.white,
                 // padding: const EdgeInsets.all(10.0), // not used for card
-                margin: const EdgeInsets.symmetric(
-                    horizontal: 25.0, vertical: 10.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: const Row(
-                    children: const [
-                      Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                        size: 40.0,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        '+91 7903205886',
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            color: Colors.teal,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Shrikhand'),
-                      )
-                    ],
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 30.0,
+                    color: Colors.teal,
                   ),
+                  title: Text(
+                    "+917903205886",
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal,
+                        fontFamily: 'Pacifico',
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+
+                // child: Padding(
+                //   padding: const EdgeInsets.all(10.0),
+                //   child: Row(
+                //     children: const [
+                //       Icon(
+                //         Icons.phone,
+                //         color: Colors.teal,
+                //         size: 40.0,
+                //       ),
+                //       SizedBox(
+                //         width: 10.0,
+                //       ),
+                //       Text(
+                //         '+91 7903205886',
+                //         style: TextStyle(
+                //             fontSize: 30.0,
+                //             color: Colors.teal,
+                //             fontWeight: FontWeight.bold,
+                //             fontFamily: 'Shrikhand'),
+                //       )
+                //     ],
+                //   ),
+                // ),
+              ),
+              SizedBox(
+                height: 20.0,
+                child: Divider(
+                  color: Colors.white38,
                 ),
               ),
               Card(
-                color: Colors.white,
+                // card is always white by default
+                // color: Colors.white,
                 // padding: const EdgeInsets.all(10.0), //not used for card
-                margin: const EdgeInsets.symmetric(
-                    horizontal: 25.0, vertical: 10.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: const Row(
-                    children: [
-                      Icon(
-                        Icons.email,
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 30.0,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "niralanitrogensir@gmail.com",
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
                         color: Colors.teal,
-                        size: 40.0,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        'abcd@gmail.com',
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            color: Colors.teal,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Shrikhand'),
-                      )
-                    ],
+                        fontFamily: 'Shrikhand'),
                   ),
                 ),
+                // child: Padding(
+                //   padding: const EdgeInsets.all(10.0),
+                //   child: Row(
+                //     children: const [
+                //       Icon(
+                //         Icons.email,
+                //         color: Colors.teal,
+                //         size: 40.0,
+                //       ),
+                //       SizedBox(
+                //         width: 10.0,
+                //       ),
+                //       Text(
+                //         'abcd@gmail.com',
+                //         style: TextStyle(
+                //             fontSize: 30.0,
+                //             color: Colors.teal,
+                //             fontWeight: FontWeight.bold,
+                //             fontFamily: 'Shrikhand'),
+                //       )
+                //     ],
+                //   ),
+                // ),
               ),
-
+              SizedBox(
+                height: 20.0,
+                child: Divider(
+                  color: Colors.white38,
+                ),
+              ),
             ],
           ),
         ),
